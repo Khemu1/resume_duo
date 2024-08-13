35eab8983be2d0ba08402b2a9cf3e8881da668fa
Khemu <alghost2004@gmail.com>
2024-08-13 15:45:29 +0300

    .

--- src/utils/user/api.js
+++ src/utils/user/api.js
@@ -2,35 +2,28 @@ export async function login(formData) {
   try {
     const response = await fetch("/api/user/login", {
       method: "POST",
-      credentials: "same-origin",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
     });
 
-    // Check if the response is not OK
     if (!response.ok) {
-      let errorMessage = "Unexpected Error Occurred";
-
+      let errorMessage = { message: "Unexpected Error Occurred" };
       try {
         const errorData = await response.json();
-
-        if (errorData.message) {
-          errorMessage = errorData.message;
-        }
+        errorMessage =
+          errorData.errors || { message: errorData.message } || errorMessage;
       } catch (jsonError) {
-        // If JSON parsing fails, fall back to a generic message
         console.error("Error parsing JSON:", jsonError);
       }
-      throw new Error(errorMessage);
+      throw errorMessage;
     }
-    // If response is OK, parse the data
-    const data = await response.json();
-    return data;
+
+    const responseData = await response.json();
+    return responseData;
   } catch (error) {
     console.error("Error:", error);
-    // Re-throw the error object so it can be caught in the hook
     throw error;
   }
 }
