30f1df69a37f6b086b59d0dc5cbe03def51e8608
Khemu <alghost2004@gmail.com>
2024-08-13 12:28:03 +0300

    .

--- src/hooks/user.js
+++ src/hooks/user.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { registerUser, Login } from "../utils/user/api";
+import { registerUser, login } from "../utils/user/api";
 
 export function useLogin() {
   const [loading, setLoading] = useState(false);
@@ -14,7 +14,7 @@ export function useLogin() {
       setLoading(true);
       setError(null);
 
-      const response = await Login(data);
+      const response = await login(data);
 
       setSuccess(true);
       console.log("Login successful:", response);
