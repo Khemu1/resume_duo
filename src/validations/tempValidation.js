645978e56796342974b84715e93eea11c9a843e1
abdalla-sobhy <abdallahbs9393@gmail.com>
2024-08-16 20:10:18 +0300

    .

--- src/validations/tempValidation.js
+++ src/validations/tempValidation.js
@@ -1,8 +1,8 @@
 import * as yup from 'yup'
 
 export const userSchema = yup.object().shape({
-    name : yup.string().required(),
-    email : yup.string().email().required(),
+    name : yup.string(),
+    email : yup.string().email(),
 },{
     abortEarly : false
 })
