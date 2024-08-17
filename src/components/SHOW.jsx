51b1a3232aac39699341bbba1c1403f1711a4148
abdalla-sobhy <abdallahbs9393@gmail.com>
2024-08-17 19:21:54 +0300

    .

--- src/components/SHOW.jsx
+++ src/components/SHOW.jsx
@@ -1,7 +1,10 @@
+import showcss from '/public/styles/Show.module.css'
 import NavBar from "./NavBar.jsx";
 
 export default function SHOW() {
     return(
-        <NavBar />
+        <>
+            <NavBar />
+        </>
     )
 }
